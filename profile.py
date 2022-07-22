8450298da8d6fd3dd04fc2c6d5163a233d10b8c6
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-07-21 20:05:46 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -25,7 +25,7 @@ for i in range(2):
   if i == 0:
     node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
   else :
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/XXX.sh"))
+    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/observer_setup.sh"))
     
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
