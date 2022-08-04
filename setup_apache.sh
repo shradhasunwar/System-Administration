c86204b01ad124bd27cce7dfae8477c96584e6e9
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-08-04 17:03:34 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -5,7 +5,7 @@ sudo apt install -y apache2
 sleep 3m
 sudo apt install -y nfs-common
 sleep 3m
-sudo apt-get install geoip-bin
+sudo apt-get install geoip-bin -y
 sleep 2m
 sudo mkdir /var/webserver_log
 sudo mount 192.168.1.2:/var/webserver_monitor /var/webserver_log
