34ee109708ca93e74dada473ccf0000dfd71a643
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-08-03 22:50:29 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -3,5 +3,5 @@ set -x
 sudo apt update
 sudo apt install -y nfs-common
 sleep 5m
-sudo mkdir /var/webserver_log
-sudo mount 192.168.1.1:/share/log /var/webserver_log/
+sudo mkdir /var/webserver_monitor 
+sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor 
