400ff763145987f46a678a301892f50b47126b07
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-08-04 17:51:09 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -8,5 +8,7 @@ sudo chmod 775 /var/webserver_monitor
 #sudo chown nobody:nogroup /var/webserver_monitor 
 echo "/var/webserver_monitor  192.168.1.1(rw,sync,no_root_squash,no_subtree_check)" |sudo tee -a /etc/exports
 sudo systemctl restart nfs-kernel-server
-
+sleep 2m
 sudo apt-get install msmtp -y
+sleep 2m
+sudo apt-get install mailutils -y
