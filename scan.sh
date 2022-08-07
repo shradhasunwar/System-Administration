7b1334cc89bb26174083c58c57a4c982d4547158
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-08-07 01:40:41 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 #Installed geoIP as part of the server setup
-sudo egrep "Connection reset by authenticating|failed|unauthorized" /var/log/auth.log | grep -wv COMMAND | awk 'BEGIN{OFS = "\t"; ORS ="\n"} {print $12, $1" " $2" " $3 ,system("geoiplookup " $12) }' >> /var/webserver_log/unauthorized.log
+sudo egrep "Connection reset by authenticating|failed|unauthorized|Invalid user" /var/log/auth.log | grep -wv COMMAND | awk '{print $1" "$2" " $3" "$10}' |grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk 'BEGIN{OFS = "\t"} {print $1" " $2" " $3" "$4  ,system("geoiplookup -s " $4) }' >> /var/webserver_log/unauthorized.log
