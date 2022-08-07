abcd55d757da0c1c19cdc234514432d0245bc5af
Shradha Sunwar <108766215+shradhasunwar@users.noreply.github.com>
2022-08-07 01:03:31 -0400

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+sudo egrep "Connection reset by authenticating|failed|unauthorized" /var/log/auth.log | grep -wv COMMAND | awk 'BEGIN{OFS = "\t"; ORS ="\n"} {print $12, $1" " $2" " $3 ,system("geoiplookup " $12) }' >> /var/webserver_log/unauthorized.log
